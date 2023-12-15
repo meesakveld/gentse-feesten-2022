8216252f749636f0cb54eb0399fa6c6efce271ef
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2023-12-15 22:10:34 +0100

    🚀 Added API funtions that handle all API data fetching

---
+++ static/js/api.js
@@ -0,0 +1,37 @@
+const API_URL_EVENTS = 'https://www.pgm.gent/data/gentsefeesten/events.json';
+const API_URL_EVENTS_LIGHT = 'https://www.pgm.gent/data/gentsefeesten/events_500.json';
+const API_URL_CATEGORIES = 'https://www.pgm.gent/data/gentsefeesten/categories.json';
+const API_URL_NEWS = 'https://www.pgm.gent/data/gentsefeesten/news.json';
+
+async function fetchAPIData(APIlink) {
+    try {
+        const response = await fetch(APIlink);
+        const data = await response.json();
+        const json = await data
+        return json
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function loadEvents(callback) {
+    const events = await fetchAPIData(API_URL_EVENTS_LIGHT)
+    callback(events);
+}
+
+async function loadCategories(callback) {
+    const categories = await fetchAPIData(API_URL_CATEGORIES)
+    callback(categories);
+}
+
+async function loadNews(callback) {
+    const news = await fetchAPIData(API_URL_NEWS)
+    callback(news);
+}
+
+
+export {
+    loadEvents,
+    loadCategories,
+    loadNews
+}
