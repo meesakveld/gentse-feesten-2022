c70ad971221c2848e8f2809b7bdf54ba042f1781
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2023-12-17 19:27:34 +0100

    🚀 Added function to load the latest three news articles

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
-import { loadEvents } from "./api.js";
+import { loadEvents, loadNews } from "./api.js";
 import { addElementToDOM, fullDaynameToShortForm, backToRoot } from "./helpers.js"
 
 function loadEightRandomEvents() {
@@ -11,21 +11,20 @@ function loadEightRandomEvents() {
         for (let i = 0; i < 8; i++) {
             events.push(eventsWithImages[Math.floor(Math.random() * eventsWithImages.length)])
         }
-        console.log(events);
 
         // Add events to the div
         const html = events.map((event, index) => {
             return `   
-            <article class="activity box ${index % 2 === 0 ? '' : 'small'}">
-                <div class="image" style="background-image: url(${event.image.full});"></div>
-                <p class="date">${fullDaynameToShortForm(event.day_of_week)} ${event.day} juli</p>
-                <a href="${backToRoot()}events/day.html?id=${event.id}" class="content">
-                    <h3 class="name">${event.title}</h3>
-                    <p class="location">${event.location}</p>
-                    <p class="time">${event.start} u.</p>
-                    <p class="price">${event.ticket === 'paid' ? '€' : ''}</p>
-                </a>
-            </article>
+                <article class="activity box ${index % 2 === 0 ? '' : 'small'}">
+                    <div class="image" style="background-image: url(${event.image.full});"></div>
+                    <p class="date">${fullDaynameToShortForm(event.day_of_week)} ${event.day} juli</p>
+                    <a href="${backToRoot()}events/day.html?id=${event.id}" class="content">
+                        <h3 class="name">${event.title}</h3>
+                        <p class="location">${event.location}</p>
+                        <p class="time">${event.start} u.</p>
+                        <p class="price">${event.ticket === 'paid' ? '€' : ''}</p>
+                    </a>
+                </article>
             `
         }).join('');
         
@@ -34,9 +33,27 @@ function loadEightRandomEvents() {
     })
 }
 
+function loadLatestNewsItems() {
+    loadNews((data) => {
+        const latestThreeArticles = data.slice(0, 3)
+        const html = latestThreeArticles.map(article => {
+            return `
+                <a href="${backToRoot()}news.html?id=${article.id}" class="btn black hover-red">
+                    <H3>${article.title}</H3>
+                    <div class="arrow right">
+                        <svg class="arrow-right" viewBox="0 0 1197 269" aria-hidden="true"><path d="M-0.159,111.093l639.756,0l-85.15,-76.855l29.643,-32.816l144.777,131.216l-143.608,129.655l-30.23,-32.081l84.144,-76.315l-639.756,0l0.424,-42.804Z" fill="#000"></path></svg>
+                    </div>
+                </a>
+            `
+        }).join('');
+
+        addElementToDOM(html, '.news__articles')
+    })
+}
 
 function init() {
     loadEightRandomEvents()
+    loadLatestNewsItems()
 }
 
 init()
