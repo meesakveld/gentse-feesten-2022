82242998a405e19185acdb00ad665d74a1a54fa9
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2023-12-19 13:31:13 +0100

    🚀 Moved function to switch view mode to main.js

--- static/js/search.js
+++ static/js/search.js
@@ -50,41 +50,8 @@ function loadEventsWhenSearched() {
     });
 }
 
-function switchViewMode() {
-    const $resultsElement = document.querySelector('.search-results__results')
-    const $boxElementSwitch = document.querySelector('span.box')
-    const $listElementSwitch = document.querySelector('span.list')
-
-    $boxElementSwitch.addEventListener('click', () => {
-        $boxElementSwitch.classList.add('active')
-        $listElementSwitch.classList.remove('active')
-        
-        const $articlesElements = document.querySelectorAll('article .activity')
-        $articlesElements.forEach(article => {
-            $resultsElement.classList.add('box')
-
-            article.classList.remove('list')
-            article.classList.add('box')
-        })
-    })
-
-    $listElementSwitch.addEventListener('click', () => {
-        $listElementSwitch.classList.add('active')
-        $boxElementSwitch.classList.remove('active')
-        
-        const $articlesElements = document.querySelectorAll('article .activity')
-        $articlesElements.forEach(article => {
-            $resultsElement.classList.remove('box')
-
-            article.classList.remove('box')
-            article.classList.add('list')
-        })
-    })
-}
-
 function init() {
     loadEventsWhenSearched()
-    switchViewMode()
 }
 
 init()
