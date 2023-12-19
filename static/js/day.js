3bed3051bae3a53be872c09d289c57f2fd23ea34
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2023-12-19 15:14:13 +0100

    🚀 Added JS function to use for day.html

---
+++ static/js/day.js
@@ -0,0 +1,130 @@
+import { loadEvents, loadCategories } from "./api.js";
+import { addElementToDOM, backToRoot, getSearchParamsFromURL, stringToLowercaseSnakeCase } from "./helpers.js"
+
+function getDateFromURL() {
+    const day = getSearchParamsFromURL('day');
+    if (day === null) { 
+        return '14'
+    } else {
+        return day
+    }
+}
+
+function checkActiveDay(id) {
+    if (id.toString() === getDateFromURL()) {
+        return 'class="active-day"'
+    } else {
+        return ''
+    }
+}
+
+function loadCalendarView() {
+    const html = `
+        <li ${checkActiveDay(14)}><a href="${backToRoot()}events/day.html?day=14"><strong>Vr</strong>14 juli</a></li>
+        <li ${checkActiveDay(15)}><a href="${backToRoot()}events/day.html?day=15"><strong>Za</strong>15 juli</a></li>
+        <span></span>
+        <li ${checkActiveDay(16)}><a href="${backToRoot()}events/day.html?day=16"><strong>Zo</strong>16 juli</a></li>
+        <li ${checkActiveDay(17)}><a href="${backToRoot()}events/day.html?day=17"><strong>Ma</strong>17 juli</a></li>
+        <li ${checkActiveDay(18)}><a href="${backToRoot()}events/day.html?day=18"><strong>Di</strong>18 juli</a></li>
+        <li ${checkActiveDay(19)}><a href="${backToRoot()}events/day.html?day=19"><strong>Wo</strong>19 juli</a></li>
+        <li ${checkActiveDay(20)}><a href="${backToRoot()}events/day.html?day=20"><strong>Do</strong>20 juli</a></li>
+        <li ${checkActiveDay(21)}><a href="${backToRoot()}events/day.html?day=21"><strong>Vr</strong>21 juli</a></li>
+        <span></span>
+        <li ${checkActiveDay(22)}><a href="${backToRoot()}events/day.html?day=22"><strong>Za</strong>22 juli</a></li>
+        <li ${checkActiveDay(23)}><a href="${backToRoot()}events/day.html?day=23"><strong>Zo</strong>23 juli</a></li>
+    `
+
+    addElementToDOM(html, '.calendar-view')
+}
+
+function loadThreeRandomActivities() {
+    loadEvents((data) => {
+        const today = getDateFromURL()
+        const events = data.filter(event => event.day === today).filter(event => event.image !== null)
+        const arr = []
+        for (let i = 0; i < 3; i++) {
+            arr.push(events[Math.floor(Math.random() * events.length)])
+        }
+
+        const html = arr.map((event, index) => {
+            return `
+                <article class="activity box ${index % 2 === 0 ? 'small' : ''}">
+                    <div class="image" style="background-image: url(${event.image.full});"></div>
+                    <a href="${backToRoot()}events/detail.html?id=${event.id}" class="content">
+                        <h3 class="name">${event.title}</h3>
+                        <p class="location">${event.location}</p>
+                        <p class="time">${event.start} u.</p>
+                        <p class="price">${event.ticket === 'paid' ? '€' : ''}</p>
+                    </a>
+                </article>
+            `
+        }).join('');
+
+        addElementToDOM(html, '.threeRandomActivities')
+    })
+}
+
+function loadCategoriesInFilterMenu() {
+    loadCategories((data) => {
+        const html = data.sort().map(category => {
+            return `
+                <li><a href="${backToRoot()}events/day.html#${stringToLowercaseSnakeCase(category)}">${category}</a></li>
+            `
+        }).join('');
+
+        addElementToDOM(html, '.filter__options-specific')
+    })
+}
+
+function loadDayEventsBasedOnCategory() {
+    const $searchResultsResultsElement = document.querySelector('.search-results__results')
+
+    loadEvents((data) => {
+        const todaysEvents = data.filter(event => event.day === getDateFromURL())
+        console.log(todaysEvents);
+
+        loadCategories(categories => {
+
+            for (const category of categories.sort()) {
+
+                const categoryEvents = todaysEvents.filter(event => event.category[0] === category)
+                console.log(categoryEvents);
+
+                if (categoryEvents.length !== 0) {
+
+                    const events = categoryEvents.map((event, index) => {
+                        return `
+                            <article class="activity box ${index % 2 === 0 ? 'small' : ''}">
+                                <div class="image" style="background-image: url(${event.image.full});"></div>
+                                <a href="${backToRoot()}events/detail.html?id=${event.id}" class="content">
+                                    <h3 class="name">${event.title}</h3>
+                                    <p class="location">${event.location}</p>
+                                    <p class="time">${event.start} u.</p>
+                                    <p class="price">${event.ticket === 'paid' ? '€' : ''}</p>
+                                </a>
+                            </article>
+                        `
+                    }).join('');
+
+                    $searchResultsResultsElement.innerHTML += `
+                        <div class="filtered-events-section">
+                            <h2><a id="${stringToLowercaseSnakeCase(category)}"></a>${category}</h2>
+                            ${events}
+                        </div>
+                    `
+                }
+            }
+        })
+    })
+}
+
+
+
+function init() {
+    loadCalendarView()
+    loadThreeRandomActivities()
+    loadCategoriesInFilterMenu()
+    loadDayEventsBasedOnCategory()
+}
+
+init()
