b4260ee582aa5511ed3709af647e271b27b692e0
pgm-meesakveld <mees.akveld@student.arteveldehs.be>
2024-01-13 18:27:03 +0100

    🚀 Updated detail page heading to include day of the week

--- static/js/pages/detail.js
+++ static/js/pages/detail.js
@@ -35,7 +35,7 @@ function generateHTMLForDetail(ev) {
         <div class="go-back">
             <a class="back arrow" href="${backToRoot()}events/day.html?day=${ev.day}">
                 <svg class="arrow-left" viewBox="0 0 1197 269" aria-hidden="true"><path d="M-0.159,111.093l639.756,0l-85.15,-76.855l29.643,-32.816l144.777,131.216l-143.608,129.655l-30.23,-32.081l84.144,-76.315l-639.756,0l0.424,-42.804Z" fill="#fff"></path></svg>
-                <p>Overzicht vrijdag ${ev.day} juli</p>
+                <p>Overzicht ${ev.day_of_week} ${ev.day} juli</p>
             </a>
         </div>
 
